6373cb5c8a9f557f8e124739273bc1984e9db650
zdrzalik-przemek <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-03-30 01:52:53 +0200

    added comments

--- zad1/main.py
+++ zad1/main.py
@@ -9,6 +9,8 @@ class NeuralNetwork:
     def __str__(self):
         return str(self.hidden_layer) + "\n" + str(self.output_layer)
 
+    # liczba neuronów w warstwie ukrytej, liczba neuronów na wyjściu, liczba inputów
+    # Tworzymy sobie dwa matrixy
     def __init__(self, number_of_neurons_hidden_layer, number_of_neurons_output, number_of_inputs):
         self.hidden_layer = 2 * numpy.random.random((number_of_inputs, number_of_neurons_hidden_layer)) - 1
         self.output_layer = 2 * numpy.random.random((number_of_neurons_hidden_layer, number_of_neurons_output)) - 1
@@ -16,32 +18,38 @@ class NeuralNetwork:
     def funkcja_sigmoidalna(self, inputcik):
             return 1 / (1 + numpy.exp(-inputcik))
 
+    # z wolfram alpha
     def pochodna_funkcja_sigmoidalna(self, inputcik):
-        return inputcik * (1 - inputcik)
+        return numpy.exp(-inputcik) /  ((numpy.exp(-inputcik) + 1) ** 2)
 
+    # najpierw liczymy wynik z warstwy ukrytej i potem korzystając z niego liczymy wynik dla neuronów wyjścia
     def calculate_outputs(self, inputs):
         hidden_layer_output = self.funkcja_sigmoidalna(numpy.dot(inputs, self.hidden_layer))
         output_layer_output = self.funkcja_sigmoidalna(numpy.dot(hidden_layer_output, self.output_layer))
         return hidden_layer_output, output_layer_output
 
+    #trening, tyle razy ile podamy epochów
     def train(self, inputs, expected_outputs, epoch_count):
         for it in range(epoch_count):
             hidden_layer_output, output_layer_output = self.calculate_outputs(inputs)
 
+            # korzystamy z pochodnej aby policzyć output_delta który wykorzystujemy potem do wyliczenia zmiany wag
             output_error = expected_outputs - output_layer_output
             output_delta = output_error * self.pochodna_funkcja_sigmoidalna(output_layer_output)
 
-
+            # tutaj jakieś czary mary totalne, patrzymy w jakiś magiczny sposób jak bardzo na błąd w warstwie zewnętrznej
+            # miały wpływ wagi w warstwie ukrytej
             hidden_layer_error = output_delta.dot(self.output_layer.T)
             hidden_layer_delta = hidden_layer_error * self.pochodna_funkcja_sigmoidalna(hidden_layer_output)
 
+            # wyliczamy zmianę wag
             hidden_layer_adjustment = inputs.T.dot(hidden_layer_delta)
             output_layer_adjustment = hidden_layer_output.T.dot(output_delta)
 
             self.hidden_layer += hidden_layer_adjustment
             self.output_layer += output_layer_adjustment
 
-
+# funkcja zwraca 2d array intów w postaci arraya z paczki numpy.
 def wczytajPunktyZPliku(file_name):
     two_dim_list_of_return_values = []
     with open(file_name, "r") as file:
@@ -52,7 +60,6 @@ def wczytajPunktyZPliku(file_name):
             one_dim_list.append(j)
         two_dim_list_of_return_values.append(one_dim_list)
     return numpy.asarray(two_dim_list_of_return_values)
-    pass
 
 
 def main():
