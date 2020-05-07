35a5d21829276ee27e869c0d51c19d69d7cd43e8
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-05-07 21:18:37 +0200

    added neural gas

--- Zad2/war_na_3/zad_3.py
+++ Zad2/war_na_3/zad_3.py
@@ -22,53 +22,93 @@ pomysl:
 
 # TODO:
 #   Pokonać problem martwych neuronów
-#   dodać drugi tryb działania (Gauss)
-#   dodać drugi algorytm (gaz neuronowy)
 #   dodać błąd kwantyzacji
-#   dodać zmianę parametru alfa
 #   przeczytać uważnie całą prezentację
-#   promień sąsiedztwa też ma maleć wraz z postępowaniem adaptacji
 
 
 class Kohonen:
-    def __init__(self, input_matrix, neuron_num, is_gauss, alfa=0.1, distance_still_change=1):
+    def __init__(self, input_matrix, neuron_num, is_neural_gas=False, is_gauss=True, alfa=0.6, neighbourhood_radius=0.5, epoch_count=1):
         self.neuron_num = neuron_num
         self.input_matrix = input_matrix
         self.is_gauss = is_gauss
         self.epoch_nr = 0
+        self.epoch_count = epoch_count
         self.map = np.random.normal(np.mean(input_matrix), np.std(input_matrix),
                                     size=(self.neuron_num, len(input_matrix[0])))
         self.distance_map = np.zeros_like(self.map)
         # poczatkowy wspolczynnik tego przesuwania XD nie wiem jak to nazwać, chce spac
-        self.alfa = alfa
-        # odleglosc dla tej metody, gdzie sie ma zmieniac tylko punkt i pobliskie. Trzeba zrobic to lepiej
-        self.distance_still_change = distance_still_change
+        self.max_alfa = alfa
+        self.min_alfa = 0.01
+        self.current_alfa = self.max_alfa
+        # Odleglosc od punktu zwycięskiego lub mianownik w drugiej metodzie
+        self.neighbourhood_radius_max = neighbourhood_radius
+        self.current_neighbourhood_radius = self.neighbourhood_radius_max
+        self.neighbourhood_radius_min = 0.01
+        self.current_step = 1
+        self.max_step = len(self.input_matrix) / len(self.input_matrix[0]) * self.epoch_count
+        self.is_neural_gas = is_neural_gas
 
     # TODO: poprawic, aktualnie zmienia tylko w promieniu
     def epoch(self):
         np.random.shuffle(self.input_matrix)
-        counter = 0
-        for i in self.input_matrix:
-            if not self.is_gauss:
-                self.distance_map_fill(i)
-                smallest_index = np.argmin(self.distance_map)
-                for j in range(len(self.map)):
-                    if distance.euclidean(self.map[j], self.map[smallest_index]) <= self.distance_still_change:
-                        self.map[j] = self.map[j] + self.alfa * (i - self.map[j])
-            else:
-                self.distance_map_fill(i)
-                smallest_index = np.argmin(self.distance_map)
-                for j in range(len(self.map)):
-                    self.map[j] = self.map[j] + self.alfa \
-                        * self.euclidean_func(self.map[smallest_index], self.map[j]) * (i - self.map[j])
-            counter += 1
+        if not self.is_neural_gas:
+            for i in self.input_matrix:
+                self.change_alpha()
+                self.change_neighbourhood_radius()
+                if not self.is_gauss:
+                    self.distance_map_fill(i)
+                    smallest_index = np.argmin(self.distance_map)
+                    for j in range(len(self.map)):
+                        if distance.euclidean(self.map[j], self.map[smallest_index]) <= self.current_neighbourhood_radius:
+                            self.map[j] = self.map[j] + self.current_alfa * (i - self.map[j])
 
-            if counter % 100 == 0:
-                print("Iteration in epoch nr", counter)
+                else:
+                    self.distance_map_fill(i)
+                    smallest_index = np.argmin(self.distance_map)
+                    for j in range(len(self.map)):
+                        self.map[j] = self.map[j] + self.current_alfa \
+                                      * self.euclidean_func(self.map[smallest_index], self.map[j]) * (i - self.map[j])
+
+                self.current_step += 1
+                if self.current_step % 100 == 0:
+                    print("Iteration in epoch nr", self.current_step)
+                    print("Current alfa", self.current_alfa)
+                    print("Current neighbour radius:", self.current_neighbourhood_radius)
+
+        else:
+            for i in self.input_matrix:
+                self.change_alpha()
+                self.change_neighbourhood_radius()
+                self.distance_map_fill(i)
+                distance_ranking = np.argsort(self.distance_map)
+                for j in range(len(distance_ranking)):
+                    self.map[distance_ranking[j]] = self.map[distance_ranking[j]] \
+                            + self.current_alfa * self.neural_gass_neighbour_fun(j) * (i - self.map[distance_ranking[j]])
+
+                self.current_step += 1
+                if self.current_step % 100 == 0:
+                    print("Iteration in epoch nr", self.current_step)
+                    print("Current alfa", self.current_alfa)
+                    print("Current neighbour radius:", self.current_neighbourhood_radius)
+
+    def neural_gass_neighbour_fun(self, ranking):
+        return np.exp(-ranking / self.current_neighbourhood_radius)
 
     def euclidean_func(self, pos_closest, pos_checked):
-        # TODO: nie wiem w jakim zakresie dac ten self.distance_sill_change ktory wgl trzeba inaczej nazwac
-        return np.exp(-distance.euclidean(pos_checked, pos_closest) ** 2 / (2 * (self.distance_still_change ** 2)))
+        return np.exp(
+            -distance.euclidean(pos_checked, pos_closest) ** 2 / (2 * (self.current_neighbourhood_radius ** 2)))
+
+    def change_neighbourhood_radius(self):
+        self.current_neighbourhood_radius = self.neighbourhood_radius_max \
+                                            * (self.neighbourhood_radius_min / self.neighbourhood_radius_max) \
+                                            ** (self.current_step / self.max_step)
+
+    def change_alpha(self):
+        self.current_alfa = self.max_alfa * (self.min_alfa / self.max_alfa) ** (self.current_step / self.max_step)
+
+    def train(self):
+        for i in range(self.epoch_count):
+            self.epoch()
 
     def distance_map_fill(self, point):
         # TODO zmien nazwe zmiennej
@@ -113,9 +153,10 @@ def plot(list2d, list2d2=None):
 
 
 def main():
-    kohonen = Kohonen(read_2d_float_array_from_file("Danetestowe.txt", is_comma=True), 100, True)
+    kohonen = Kohonen(input_matrix=read_2d_float_array_from_file("Danetestowe.txt", is_comma=True),
+                      neuron_num=200, is_neural_gas=True, epoch_count=1)
     plot(kohonen.map, read_2d_float_array_from_file("Danetestowe.txt", is_comma=True))
-    kohonen.epoch()
+    kohonen.train()
     plot(kohonen.map, read_2d_float_array_from_file("Danetestowe.txt", is_comma=True))
     # kohonen = Kohonen(read_2d_float_array_from_file("punkty.txt", is_comma=False), 1000, True)
     # plot(kohonen.map, read_2d_float_array_from_file("punkty.txt", is_comma=False))
